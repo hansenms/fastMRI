5de9cadd4a6a8887b870f77fbf35d6df6ad06599
Michael Hansen <mihansen@microsoft.com>
2021-01-12 21:41:55 +0000

    Robustness for port selection

--- fastmri_examples/varnet/azureml_env_adapter.py
+++ fastmri_examples/varnet/azureml_env_adapter.py
@@ -10,10 +10,10 @@ def set_environment_variables():
     else:
         raise Exception("No relevant MASTER_ADDR candidates found in environment. Please set AZ_BATCHAI_MPI_MASTER_NODE or MASTER_IP")
 
-    os.environ["MASTER_PORT"] = "6105"
-    os.environ["NODE_RANK"] = os.environ[
-        "OMPI_COMM_WORLD_RANK"
-    ]  # node rank is the world_rank from mpi run
+    if "MASTER_PORT" not in os.environ:
+        os.environ["MASTER_PORT"] = "6105"
+
+    os.environ["NODE_RANK"] = os.environ["OMPI_COMM_WORLD_RANK"]  # node rank is the world_rank from mpi run
     print("MASTER_ADDR = {}".format(os.environ["MASTER_ADDR"]))
     print("MASTER_PORT = {}".format(os.environ["MASTER_PORT"]))
     print("NODE_RANK = {}".format(os.environ["NODE_RANK"]))
